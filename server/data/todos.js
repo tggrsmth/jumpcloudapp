8bfe8dee0dff14bb3666e755172f6c569be5cda1
Mike DiLorenzo <mikedilorenzo@gmail.com>
2019-08-07 14:24:39 -0600

    Proxy requests from localhost:8005/api to console.jumpcloud.com/api

--- server/data/todos.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-  { id: 1, done: false, description: 'Pick up eggs' },
-  { id: 2, done: false, description: 'Pay electric bill' },
-  { id: 3, done: true, description: 'Create todo API' }
-];
--- server/server.js
+++ server/server.js
@@ -1,40 +1,37 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var PORT = 8004;
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const dotenv = require('dotenv');
+const app = express();
+const PORT = 8004;
 
-app.use(bodyParser.json());
+dotenv.config();
+
+const API_KEY = process.env.API_KEY;
 
 app.use(function(req, res, next) {
-  var whitelist = [
-    'http://localhost:8005',
-  ];
+  const origin = req.headers.origin;
 
-  var origin = req.headers.origin;
-
-  if (whitelist.indexOf(origin) > -1) {
+  if (String(origin).includes('http://localhost')) {
     res.setHeader('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
   }
 
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    res.send();
+    return;
+  }
+
   return next();
 });
 
-var data = {
-  todos: require('./data/todos')
-};
-
-var controllers = {
-  todos: require('./controllers/todos')(data.todos)
-};
-
-// ToDo API routes
-app.get('/api/todos', controllers.todos.getAll);
-app.get('/api/todos/:id', controllers.todos.getOne);
-app.post('/api/todos', controllers.todos.create);
-app.put('/api/todos/:id', controllers.todos.update);
-app.delete('/api/todos/:id', controllers.todos.delete);
+app.use('/api', proxy({
+  target: 'https://console.jumpcloud.com',
+  changeOrigin: true,
+  headers: {
+    'x-api-key': API_KEY,
+  },
+}));
 
 app.listen(PORT, function() {
   console.log('Server started on port ' + PORT);
